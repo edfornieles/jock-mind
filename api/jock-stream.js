0f570ea471c00539b73180ca7fd1de0ade317045
edfornieles <edward.fornieles@gmail.com>
2025-05-06 17:19:13 +0100

    Update jock-stream.js

--- api/jock-stream.js
+++ api/jock-stream.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
